ccedbce7c933bb2a82e3802b1e7e9392bb79c498
bhatiamanan1 <108178767+bhatiamanan1@users.noreply.github.com>
2023-05-03 02:24:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Make Palindrome/make-palindrome.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    bool makePalindrome(int n,vector<string> &arr){
+        // Code here
+         map<string, int> mp;
+         for(int i=0;i<n;i++){
+             mp[arr[i]]++;
+             string str="";
+             str = arr[i];
+             reverse(str.begin(),str.end());
+             mp[str]--;
+         }
+         for(auto it:mp){
+             if(it.second%2==1){
+                 return false;
+             }
+         }
+         return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        if(ob.makePalindrome(n,arr)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

b57a76e3e527c85be2765799077c328357366a91
bhatiamanan1 <108178767+bhatiamanan1@users.noreply.github.com>
2023-06-23 23:57:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Task Scheduler/task-scheduler.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int leastInterval(int N, int K, vector<char> &tasks) {
+        // code here
+        int CHAR[26] = {0};
+        int maxF = -1;
+        for(int i=0; i<N; i++) {      
+            CHAR[tasks[i]-'A']++;
+            maxF = max(maxF, CHAR[tasks[i]-'A']);
+        }
+        int extraTime = 0;   
+        for(int i=0; i<26; i++){          
+            if(CHAR[i] == maxF) extraTime += 1;
+        }
+        int totalTime = K * (maxF-1) + extraTime-1 + maxF;
+        return max(N, totalTime); 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<char> tasks(N);
+        for (int i = 0; i < N; i++) cin >> tasks[i];
+
+        Solution obj;
+        cout << obj.leastInterval(N, K, tasks) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

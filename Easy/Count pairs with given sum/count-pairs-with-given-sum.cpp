bd69ceca4657ed3d1dc9aea4bc64e178274146cc
bhatiamanan1 <108178767+bhatiamanan1@users.noreply.github.com>
2023-07-08 01:01:37 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -11,9 +11,9 @@ class Solution{
 public:
     int getPairsCount(int arr[], int n, int k) {
         // code here
-        int count=0;
-        map<int, int> mp;
         
+        int count=0;
+        map<int, int>mp;
         for(int i=0;i<n;i++){
             if(mp.find(k-arr[i])!=mp.end()){
                 count+=mp[k-arr[i]];

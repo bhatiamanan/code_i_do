8015150c39bb8315f9686f8ae6bfc1f0861c5f82
bhatiamanan1 <108178767+bhatiamanan1@users.noreply.github.com>
2023-09-10 01:29:33 +0530

    Updated Solution - GfG to GitHub

--- Easy/Level order traversal/level-order-traversal.cpp
+++ Easy/Level order traversal/level-order-traversal.cpp
@@ -42,26 +42,23 @@ class Solution
 {
     public:
     //Function to return the level order traversal of a tree.
-    vector<int> levelOrder(Node* node)
+    vector<int> levelOrder(Node* root)
     {
       //Your code here
-      vector<int> v;
-      queue<Node*> q;
-      q.push(node);
-      
-      while(!q.empty()){
-          Node* temp = q.front();
-          v.push_back(temp->data);
-          q.pop();
-          
-          if(temp->left) q.push(temp->left);
-          if(temp->right) q.push(temp->right);
-          
-      }
-      return v;
+       vector<int> v;
+       queue<Node*> q;
+       q.push(root);
+       
+       while(!q.empty()){
+           Node* temp = q.front();
+           v.push_back(temp->data);
+           q.pop();
+           if(temp->left) q.push(temp->left);
+           if(temp->right) q.push(temp->right);
+       }
+       return v;
     }
 };
-
 
 //{ Driver Code Starts.
 

4a3e01dfc9110abf3db51df4707fadceaa105f5c
bhatiamanan1 <108178767+bhatiamanan1@users.noreply.github.com>
2023-09-16 00:06:25 +0530

    Updated Solution - GfG to GitHub

--- Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
+++ Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -9,25 +9,32 @@ using namespace std;
 
 class Solution{
 public:
-    int equalPartition(int n, int a[]){
-        int sum = 0; 
-        for(int i = 0; i < n; i++)
-            sum += a[i];
-        if(sum % 2 != 0)
-            return 0;
+
+    bool solveMem(int index, int n, int arr[],int target, vector<vector<int>>& dp){
+        if(index>=n || target<0) return 0;
+        if(target==0) return 1;
         
-        int dp[sum/2 + 1]; memset(dp, 0, sizeof(dp));
-        dp[0] = 1;
-            
-        for(int i = n - 1; i >= 0; i--){
-            for(int target = sum/2; target >= 0; target--){
-                if(target >= a[i])
-                    dp[target] |= dp[target - a[i]];
-            }
+        if(dp[index][target]!= -1) return dp[index][target];
+        
+        bool incl = solveMem(index+1, n, arr, target-arr[index], dp);
+        bool excl = solveMem(index+1, n, arr, target, dp);
+        
+        return dp[index][target] =  incl||excl;
+    }
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum = 0;
+        for(int i=0;i<N;i++){
+            sum+=arr[i];
         }
-        return dp[sum/2];
+        if(sum%2!=0) return 0;
+        int target = sum/2;
+        vector<vector<int>>dp(N, vector<int>(target+1, -1));
+        return solveMem(0, N, arr, target, dp);
     }
 };
+
 
 //{ Driver Code Starts.
 

d5b2d80835f0b3ac72dbe28d9c1c95144459b78c
Manan Bhatia <108178767+bhatiamanan@users.noreply.github.com>
2024-06-13 23:26:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int padovanSequence(int n) {
+        // code here.
+        if(n<=2) return 1;
+        int a=1,b=1,c=1,mod=1e9+7,temp=0;
+        for(int i=3;i<=n;i++){
+            temp=(a+b)%mod;
+            a=b;
+            b=c;
+            c=temp;
+        }
+        return c;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.padovanSequence(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

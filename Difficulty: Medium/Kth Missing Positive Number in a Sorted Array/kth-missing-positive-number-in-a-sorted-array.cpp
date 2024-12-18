ad0b5d2cd8df49021d5df08ad98c274523542af3
Manan Bhatia <108178767+bhatiamanan@users.noreply.github.com>
2024-12-19 01:39:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int kthMissing(vector<int> &arr, int k) {
+        // Your code goes here
+        int n = arr.size();
+        int missingCount = 0, lastNum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currentMissing = arr[i] - lastNum - 1;
+            if (k <= missingCount + currentMissing) return lastNum + k - missingCount;
+            missingCount += currentMissing;
+            lastNum = arr[i];
+        }
+        // If kth missing is beyond the last element of the array
+        return lastNum + (k - missingCount);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.kthMissing(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

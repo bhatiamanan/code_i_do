2dfe70cc119caef1a1154c70a07178fd03ef9d2a
Manan Bhatia <108178767+bhatiamanan@users.noreply.github.com>
2025-03-22 15:46:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -12,13 +12,16 @@ class Solution {
     
     int nthFibonacci(int n) {
         // code here
-        vector<int> dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
+        if(n<2) return n;
+        int justPrev=1;
+        int prevBeforePrev=0;
+        int num = 0;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            num = justPrev + prevBeforePrev;
+            prevBeforePrev = justPrev;
+            justPrev = num;
         }
-        return dp[n];
+        return num;
     }
 };
 

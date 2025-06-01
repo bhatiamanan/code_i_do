c7e54eabc500f52bf8f3cae0bc7e70605d53a9e4
Manan Bhatia <108178767+bhatiamanan@users.noreply.github.com>
2025-06-01 16:50:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
+++ Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -2,22 +2,36 @@ class Solution {
   public:
     int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int x) {
         // code here
-        map<int, int> mp;
+        
+        //two-pointer
         int n = mat1.size();
+        int r1 = 0;
+        int r2 = n-1;
+        int c1 = 0;
+        int c2 = n-1;
         
-        int ans = 0;
+        int count = 0;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                mp[x-mat1[i][j]]++;
+        while(r1<n && r2>=0){
+            int pairSum = mat1[r1][c1]+mat2[r2][c2];
+            
+            if(pairSum == x){
+                count++;
+                c1++;
+                c2--;
+            }else if(pairSum>x) c2--;
+            else c1++;
+            
+            if(c1==n){
+                c1 = 0;
+                r1++;
+            }
+            if(c2==-1){
+                c2=n-1;
+                r2--;
             }
         }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(mp.find(mat2[i][j])!=mp.end()) ans+=mp[mat2[i][j]];
-            }
-        }
-        return ans;
+        return count;
     }
 };
